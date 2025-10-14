c7a886127f53245080253afd160c079db2b2e540
inxeoz <inxeoz@inxeoz.com>
2025-10-14 16:04:00 +0530

    get_attender_appointments_list

--- mahakaal/darshan_booking/doctype/darshan_attender_profile/darshan_attender_profile.py
+++ mahakaal/darshan_booking/doctype/darshan_attender_profile/darshan_attender_profile.py
@@ -146,20 +146,20 @@ def get_self_profile():
 
 @frappe.whitelist()
 @_ensure_role(PROFILE_ROLE)
-def get_attender_appointments_list(date:str=None):
+def get_attender_appointments_list(appointment_date:str=None):
     
     current_user_id = frappe.session.user
     attender_profile_id = frappe.db.exists(PROFILE_TYPE, {'frappe_profile' : current_user_id})
 
-    if date is None:
-        date = date.today().strftime("%Y-%m-%d")
+    if appointment_date is None:
+        appointment_date = date.today().strftime("%Y-%m-%d")
     else:
-        date = frappe.utils.getdate(date)
+        appointment_date = frappe.utils.getdate(appointment_date)
 
     schedules = frappe.get_all(
             'Attender Schedule Table',
             filters={
-                'appointment_date': date,
+                'appointment_date': appointment_date,
             },
             fields=['appointment_date', 'appointment_type', 'slot_start_time', 'slot_end_time', 'appointment', 'name'] 
         )
