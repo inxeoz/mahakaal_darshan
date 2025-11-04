301b92ed0d8ecc541b5277b70a0461ba6bb7b39a
Purushottam Singram <87688500+inxeoz@users.noreply.github.com>
2025-11-04 15:17:57 +0530

    Update issue.py

--- mahakaal/mahakaal/issue.py
+++ mahakaal/mahakaal/issue.py
@@ -1,11 +1,11 @@
-import frappe
+# import frappe
 
-def after_insert(doc, method):
-    message = f"[Mahakaal] New issue created: {doc.name} --from HOOKS"
-    print(message)  # shows in `bench start` terminal
-    frappe.logger().info(message)  # logs to logs/frappe.log
+# def after_insert(doc, method):
+#     message = f"[Mahakaal] New issue created: {doc.name} --from HOOKS"
+#     print(message)  # shows in `bench start` terminal
+#     frappe.logger().info(message)  # logs to logs/frappe.log
 
-def on_update(doc, method):
-    message = f"[Mahakaal] Issue updated: {doc.name}, status={doc.status} --from HOOKS"
-    print(message)
-    frappe.logger().info(message)
+# def on_update(doc, method):
+#     message = f"[Mahakaal] Issue updated: {doc.name}, status={doc.status} --from HOOKS"
+#     print(message)
+#     frappe.logger().info(message)
